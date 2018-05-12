8e6123d04cc4399971279da5b17b803ab8550e08
Oleg Polyakov <olegpolyako@outlook.com>
2018-05-12 19:38:49 +0300

    End

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,26 @@ import React from 'react';
 import Counter from './components/Counter';
 
 export default class App extends React.Component {
+    state = {
+        count: 1
+    }
+
+    handleChange = count =>
+        this.setState(
+            state => ({ count }),
+            () => console.log('APP', this.state)
+        );
+
     render() {
         return (
-            <Counter />
+            <React.Fragment>
+                <Counter onChange={this.handleChange} />
+
+                <Counter
+                    count={this.state.count}
+                    onChange={this.handleChange}
+                />
+            </React.Fragment>
         );
     }
 }
--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -4,18 +4,35 @@ import './index.css';
 
 class Counter extends React.Component {
     state = {
-        count: 0
+        count: this.props.count || 0
     };
 
-    increment = () => this.setState(state => ({ count: state.count + 1 }));
-    decrement = () => this.setState(state => ({ count: state.count - 1 }));
+    get isControlled() {
+        return this.props.count !== undefined;
+    }
+
+    getState() {
+        return this.isControlled ? this.props : this.state;
+    }
+
+    change = value => {
+        if (this.isControlled) {
+            this.props.onChange(this.getState().count + value);
+        } else {
+            this.setState(state => ({ count: this.getState().count + value }));
+        }
+    }
+
+    increment = () => this.change(1);
+
+    decrement = () => this.change(-1);
 
     render() {
         return (
             <div className="counter">
-                <button onClick={this.increment}>-</button>
-                <span>{this.state.count}</span>
-                <button onClick={this.decrement}>+</button>
+                <button onClick={this.decrement}>-</button>
+                <span>{this.getState().count}</span>
+                <button onClick={this.increment}>+</button>
             </div>
         );
     }
